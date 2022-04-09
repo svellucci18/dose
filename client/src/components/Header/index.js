18ec960b99bf3bc91026d3f7d1fe5bbd48ca1c65
Sean Ashby <sean.r.ashby@gmail.com>
2022-04-09 13:08:20 -0700

    adding header footer

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link className="text-light" to="/">
-          <h1 className="m-0">Tech Thoughts</h1>
+          <h1 className="m-0">dose</h1>
         </Link>
         <p className="m-0">Get into the mind of a programmer.</p>
       </div>
