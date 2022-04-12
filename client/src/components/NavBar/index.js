12ba58d3a0ced871fae9922bd21c1e6538d05dd2
Lo Selby <laurenxselby@gmail.com>
2022-04-11 22:36:07 -0700

    navbar styling and animations

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {  Container, Navbar, Offcanvas, Nav, Link, NavDropdown, Form, FormControl, Button  } from "react-bootstrap/";
+import {  Container, Navbar, Offcanvas, Nav, Link, NavDropdown, Form, FormControl, Button, Image, Row  } from "react-bootstrap/";
 
 import Auth from '../../utils/auth';
+
+
+// import images
+import mushroom from '../../assets/images/mushroom.png';
 
 const NavBar = () => {
   const logout = (event) => {
@@ -9,9 +13,9 @@ const NavBar = () => {
     Auth.logout();
   };
   return (
-    <Navbar bg="light" expand={false}>
+    <Navbar id="doseNav" className="py-2" expand={false}>
   <Container fluid>
-    <Navbar.Brand className= "mx-auto fs-1" href="#">dose.</Navbar.Brand>
+    <Navbar.Brand className= "mx-auto navTitle" href="#">dose.</Navbar.Brand>
     <Navbar.Toggle aria-controls="offcanvasNavbar" />
     <Navbar.Offcanvas
       id="offcanvasNavbar"
@@ -21,30 +25,27 @@ const NavBar = () => {
       
     >
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+        <Offcanvas.Title id="offcanvasNavbarLabel">
+          <Container >
+            
+              <p className="fs-6">
+                Login
+              </p>
+              <img src={mushroom} width="40" height="40" alt="mushroom"/>
+              <p className="fs-6">
+                Logout
+              </p>
+         
+          </Container>
+        </Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Something else here
-            </NavDropdown.Item>
-          </NavDropdown>
+        <Nav className="justify-content-end flex-grow-1 pe-3 ">
+          <Nav.Link href="#action1" id="home" className="whiteText fs-1 text-center py-5">Home</Nav.Link>
+          <Nav.Link href="#action2" id="learn" className="whiteText text-decoration-none fs-1 text-center pe-2">Learn</Nav.Link>
+          <Nav.Link href="#action2" id="about" className="whiteText text-decoration-none fs-1 text-center pe-2">About</Nav.Link>
+          <Nav.Link href="#action2" id="dose" className="whiteText text-decoration-none fs-1 text-center pe-2">Dose</Nav.Link>
         </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Offcanvas.Body>
     </Navbar.Offcanvas>
   </Container>
