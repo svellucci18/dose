ef2d31800cf87165227a93caa8474696d703b288
kasai <kasaipreston@gmail.com>
2022-04-16 11:28:38 -0700

    comsumable list

---
+++ client/src/components/ConsumableList/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Form, Button, Modal } from "react-bootstrap/";
+
+const ConsumableList = (props) => {
+
+  return (
+ 
+
+
+
+
+
+
+
+    
+  );
+};
+
+export default ConsumableList;
