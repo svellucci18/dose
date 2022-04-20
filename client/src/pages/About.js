1352025abffc7a82149177ec27fa44f7fe8362cb
kasai <kasaipreston@gmail.com>
2022-04-19 19:46:11 -0700

    moved css files to styles folder

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
-import './about.css';
+import '../styles/about.css';
 import {  Container, Navbar, Offcanvas, Nav, NavDropdown, Form, FormControl, Button, Image, Col, Row  } from "react-bootstrap/";
 
 
