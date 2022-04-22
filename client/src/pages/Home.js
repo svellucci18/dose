a1e3d0c5b3ad3a0805ad94f971062e290f353f67
kasai <kasaipreston@gmail.com>
2022-04-21 18:23:29 -0700

    some styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
          
           <Carousel>
             <Carousel.Item interval={5000}>
-              <img className="d-block"  src={carousel1} alt="girl smiling in nature" />
+              <img className="d-block img-fluid"  src={carousel1} alt="girl smiling in nature" />
               <Carousel.Caption className="text-bg">
                 <h3>Microdosing</h3>
                 <p>
@@ -25,7 +25,7 @@ const Home = () => {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
-              <img className="d-block" src={meditate} alt="Sman meditating in field" />
+              <img className="d-block img-fluid" src={meditate} alt="Sman meditating in field" />
 
               <Carousel.Caption className="text-bg">
                 <h3>Isolate a variable</h3>
@@ -33,7 +33,7 @@ const Home = () => {
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
-              <img className="d-block " src={enlightend} alt="man standing at edge of dock on a lake" />
+              <img className="d-block img-fluid" src={enlightend} alt="man standing at edge of dock on a lake" />
 
               <Carousel.Caption className="text-bg">
                 <h3>Record daily</h3>
@@ -51,8 +51,8 @@ const Home = () => {
         <Col m={12}>
         <Card className="mt-5 mx-auto dose" style={{height: '393px'}}>
           <Card.Body>
-            <h1 className="text-center">Welcome to <spane className="micro">dose.</spane></h1> Microdosing psychedelics is the practice of consuming very low, 
-            sub-hallucinogenic doses of a psychedelic substance, such as lysergic acid diethylamide (LSD) or psilocybin-containing mushrooms. Microdosing has seen a rapid explosion of popularity in recent years. Individuals who microdose report minimal acute effects from these substances yet claim a range of long-term general health and wellbeing benefits. With <spane className="microDose">dose.</spane> you can track your own experience with microdosing and the effects it has had on your health!
+            <h1 className="text-center">Welcome to dose.</h1> Microdosing psychedelics is the practice of consuming very low, 
+            sub-hallucinogenic doses of a psychedelic substance, such as lysergic acid diethylamide (LSD) or psilocybin-containing mushrooms. Microdosing has seen a rapid explosion of popularity in recent years. Individuals who microdose report minimal acute effects from these substances yet claim a range of long-term general health and wellbeing benefits. With Dose you can track your own experience with microdosing and the effects it has had on your health!
             
             </Card.Body>
         </Card>
