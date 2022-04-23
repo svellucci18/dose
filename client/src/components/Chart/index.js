1c49b2536c6248ae655f17da63e463fc4f981f62
Michael Iringan <miringan@uw.edu>
2022-04-23 14:00:19 -0700

    Added extra dataset, updated titles, added more data points.

--- client/src/components/Chart/index.js
+++ client/src/components/Chart/index.js
@@ -6,14 +6,19 @@ import { Chart as ChartJS } from "chart.js/auto";
 // Save mood data into one array
 // Save consumable data into another array
 // Save dates into another array
+// Create a helper function to add custom styling for each dataset
+// Use states to prevent chart data from loading before the data from the api has been loaded to the graphs
+// Use helper functions to create datasets
+//      But first, try to do that directly in the same file without helper functions.
+// Need to add legend
 
 // data prop for the chart
 const chartData = {
-    labels: ["Consumable 1", "Consumable 2", "Consumable 3", "Consumable 4", "Consumable 5"],
+    labels: ["4/19", "4/20", "4/21", "4/22", "4/25", "4/26", "4/27"],
     datasets: [
         {
-            label: "amount",
-            data: [12, 9, 15, 6, 7],
+            label: "Dosed",
+            data: [4, 2, 4, 2, 4, 2, 4],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -28,11 +33,23 @@ const chartData = {
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 10
         },
         {
-            label: "different amount",
-            data: [7, 6, 15, 9, 12],
+            label: "physical health",
+            data: [4, 1, 3, 2, 4, 2, 5],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 10
+        },
+        {
+            label: "mental health",
+            data: [5, 1, 1, 2, 5, 3, 4],
+            borderColor: [
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 10
         }
     ]
 }
@@ -40,17 +57,18 @@ const chartData = {
 // chart hook
 function DataChart () {
     return (<div>
-        <h1 class="d-flex justify-content-center">Data Chart</h1>
-        <Bar data={chartData}
+        <h1 class="d-flex justify-content-center">April: LSD</h1>
+        <Line data={chartData}
             options={{
                 plugins: {
                     title: {
                         display: true,
-                        text: "Sample Data"
+                        text: "Mood Tracker"
+                        
                     },
                     legend: {
                         display: true,
-                        position: "bottom"
+                        position: "top"
                     }
                 }
             }}/>
